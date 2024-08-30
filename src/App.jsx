3488f990f76c4a8433e5cc0da26a371acecd2781
chenghuiting <1365256914@qq.com>
2024-08-30 17:11:04 +0800

    创建项目

---
+++ src/App.jsx
@@ -0,0 +1,99 @@
+import React, { memo, useState } from 'react';
+import { Input, Form, Button, message } from 'antd';
+import { debounce } from 'lodash';
+import axios from 'axios';
+import AppWrap from './style';
+
+const App = memo(() => {
+  const [messageApi, contextHolder] = message.useMessage();
+  const [cardPwd, setCardPwd] = useState((window.location.search).split('=')[1])
+
+  function onFinish(values) {
+    console.log(values);
+    // 将输入的内容提交到后台
+    axios.post(`http://124.222.46.46:8900/wx/send/text/msg`, values).then(res => {
+      if (res.data.code === 0) {
+        messageApi.open({
+          type: 'success',
+          content: '提交成功！',
+          duration: 10,
+        });
+      } else {
+        messageApi.open({
+          type: 'error',
+          content: res.data.msg,
+          duration: 10,
+        });
+      }
+    })
+  }
+
+  // 防抖
+  const debounceOnFinish = debounce(onFinish, 500)
+
+
+  // 提交表单且数据验证失败后回调事件
+  function onFinishFailed(values) {
+    console.log(values);
+  }
+
+
+  return (
+    <AppWrap>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        style={{
+          maxWidth: 600,
+        }}
+        onFinish={debounceOnFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+        initialValues={{
+          cardPwd,
+        }}
+      >
+        <Form.Item
+          label="邀请链接"
+          name="inviteLink"
+          rules={[
+            {
+              required: true,
+              message: '请输入邀请链接',
+            },
+          ]}
+        >
+          <Input placeholder="请输入邀请链接" />
+        </Form.Item>
+        <Form.Item
+          label="卡密"
+          name="cardPwd"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input disabled />
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            提交
+          </Button>
+        </Form.Item>
+      </Form>
+    </AppWrap>
+  )
+})
+
+export default App
---
+++ src/style.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+const AppWrap = styled.div`
+
+
+`
+export default AppWrap
